6ffc9c9ce936362b6f816952602051e8bec1df87
EC2 Default User <ec2-user@ip-172-31-4-220.us-east-2.compute.internal>
2021-02-25 16:48:47 +0000

    img test

--- src/components/GridCardView/index.jsx
+++ src/components/GridCardView/index.jsx
@@ -4,14 +4,17 @@ import './index.css';
 import logo from '../App/logo.svg';
 
 const GridCardView = (props) => {
-  const { item: { title, details } } = props;
+  const { item: { title, details, id } } = props;
+
   return (
     <div className="card">
       <div className="container">
-        <img src={logo} alt="Movie" />
+        <img src= {`https://d1xncxi4z6qosf.cloudfront.net/${id}/${id}.0000000.jpg`} alt="Movie" />
+        {//<img src={logo} alt="Movie" />
+        }
         {title}
         {details}
-        <button type="button">"Vote!"</button>
+       
       </div>
     </div>
   );
